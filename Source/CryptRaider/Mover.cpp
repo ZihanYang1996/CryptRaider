c9bbed2b90c7202d2de033cea0d96d4b6b477049
Zihan Yang <zihan_yang_1996@outlook.com>
2024-06-12 16:37:03 -0400

    Add Mover C++ component to move an object, implemented with three different method, Lerp, VInterpTo and VInterpConstantTo. Can choose which method to use in settings.

--- Source/CryptRaider/Mover.cpp
+++ Source/CryptRaider/Mover.cpp
@@ -10,8 +10,6 @@ UMover::UMover()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 	bIsMoving = false;
-	
-	// ...
 }
 
 
@@ -20,18 +18,20 @@ void UMover::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	// Calulate the move speed for VInterpConstantTo
+	ConstantMoveSpeed = MoveOffset.Size() / MoveTime;
+	// Get the start location
+	StartLocation = GetOwner()->GetActorLocation();
 
-	MoveSpeed = MoveOffset.Size() / MoveTime;
-	// Log MoveSpeed
-	UE_LOG(LogTemp, Warning, TEXT("MoveSpeed: %f"), MoveSpeed);
-	UE_LOG(LogTemp, Warning, TEXT("MoveOffset.Size(): %f"), MoveOffset.Size());
-	UE_LOG(LogTemp, Warning, TEXT("MoveTime: %f"), MoveTime);
+	// UE_LOG(LogTemp, Warning, TEXT("MoveSpeed: %f"), ConstantMoveSpeed);
+	// UE_LOG(LogTemp, Warning, TEXT("MoveOffset.Size(): %f"), MoveOffset.Size());
+	// UE_LOG(LogTemp, Warning, TEXT("MoveTime: %f"), MoveTime);
 
 	// A delay to trigger movement
-	FTimerHandle InvokeHandle;
-	GetWorld()->GetTimerManager().SetTimer(InvokeHandle, this, &UMover::TriggerMovement, 5, false);
-	// TriggerMovement();
+	FTimerHandle InvokeHandle; // Timer handle
+	// Two ways to call TriggerMovement
+	// GetWorld()->GetTimerManager().SetTimer(InvokeHandle, this, &UMover::TriggerMovement, 5, false);
+	GetWorld()->GetTimerManager().SetTimer(InvokeHandle, this, &UMover::TriggerMovement, 1, false, 5);
 }
 
 
@@ -46,32 +46,90 @@ void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponent
 void UMover::TriggerMovement()
 {
 	UE_LOG(LogTemp, Warning, TEXT("Triggering movement!"));
+	// The target location
 	FVector TargetLocation = GetOwner()->GetActorLocation() + MoveOffset;
+	// Set the flag to indicate that the actor is moving
 	bIsMoving = true;
 
-	// Lambda function to call MoveStep with parameters
+	// Lambda function to call MoveStep that uses VInterpConstantTo or VInterpTo
 	auto MoveStepLambda = [this, TargetLocation]()
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Lambda called"));
 		this->MoveStep(TargetLocation);
 	};
 
+	// Lambda function to call MoveStep that uses Lerp (not used)
+	// Has to use a shared pointer, if declared DT as a float and captured by lambda as a reference, DT will be destroyed and out of scope after TriggerMovement is done
+	TSharedPtr<float> DT = MakeShared<float>(0.0f);
+	auto MoveStepLambda2 = [this, TargetLocation, DT]()
+	{
+		// UE_LOG(LogTemp, Warning, TEXT("Lambda called"));
+		// *DT += 1;
+		// UE_LOG(LogTemp, Warning, TEXT("Total DeltaTime: %f"), *DT);
+		this->MoveStep(TargetLocation, DT);
+	};
+
 	// Start the timer to call MoveStep every frame
-	GetWorld()->GetTimerManager().SetTimer(MoveTimerHandle, MoveStepLambda, 0.01f, true);
+	StartTime = FPlatformTime::Seconds();
+	if (MovementType == EMoverMode::Lerp)
+	{
+		GetWorld()->GetTimerManager().SetTimer(MoveTimerHandle, MoveStepLambda2, GetWorld()->GetDeltaSeconds(), true,
+		                                       0);
+	}
+	else
+	{
+		GetWorld()->GetTimerManager().SetTimer(MoveTimerHandle, MoveStepLambda, GetWorld()->GetDeltaSeconds(), true, 0);
+	}
 }
 
+// The version of MoveStep that uses VInterpConstantTo or VInterpTo
 void UMover::MoveStep(const FVector& TargetLocation)
 {
 	AActor* Owner = GetOwner();
 	FVector CurrentLocation = Owner->GetActorLocation();
-	FVector NewLocation = FMath::VInterpConstantTo(CurrentLocation, TargetLocation, GetWorld()->GetDeltaSeconds(), MoveSpeed);
-	
-	
-	UE_LOG(LogTemp, Warning, TEXT("NewLocation location: %s"), *NewLocation.ToString());
+	FVector NewLocation;
+
+	switch (MovementType)
+	{
+		// Use VInterpTo
+		case EMoverMode::VInterpTo:
+			NewLocation = FMath::VInterpTo(CurrentLocation, TargetLocation, GetWorld()->GetDeltaSeconds(), 5);
+			break;
+
+		// Use VInterpConstantTo (default)
+		case EMoverMode::VInterpConstantTo:
+		default:
+			NewLocation = FMath::VInterpConstantTo(CurrentLocation, TargetLocation, GetWorld()->GetDeltaSeconds(),
+			                                       ConstantMoveSpeed);
+			break;
+	}
 
 	Owner->SetActorLocation(NewLocation);
 	if (NewLocation.Equals(TargetLocation, 0.1f))
 	{
+		// Temp, for timer
+		double EndTime = FPlatformTime::Seconds();
+		UE_LOG(LogTemp, Warning, TEXT("Time taken: %f"), EndTime - StartTime);
+
+		bIsMoving = false;
+		GetWorld()->GetTimerManager().ClearTimer(MoveTimerHandle);
+		UE_LOG(LogTemp, Warning, TEXT("Movement complete"));
+	}
+}
+
+
+// The version of MoveStep that uses Lerp
+void UMover::MoveStep(const FVector& TargetLocation, TSharedPtr<float> DeltaTime)
+{
+	AActor* Owner = GetOwner();
+	*DeltaTime += GetWorld()->GetDeltaSeconds();
+	float Alpha = FMath::Clamp(*DeltaTime / MoveTime, 0.0f, 1.0f);
+	FVector NewLocation = FMath::Lerp(StartLocation, TargetLocation, Alpha);
+	Owner->SetActorLocation(NewLocation);
+	if (Alpha >= 1.0f)
+	{
+		// Temp
+		double EndTime = FPlatformTime::Seconds();
+		UE_LOG(LogTemp, Warning, TEXT("Time taken: %f"), EndTime - StartTime);
 		bIsMoving = false;
 		GetWorld()->GetTimerManager().ClearTimer(MoveTimerHandle);
 		UE_LOG(LogTemp, Warning, TEXT("Movement complete"));
--- Source/CryptRaider/Mover.h
+++ Source/CryptRaider/Mover.h
@@ -6,6 +6,14 @@
 #include "Components/ActorComponent.h"
 #include "Mover.generated.h"
 
+// Define the enum for the movement type
+UENUM(BlueprintType)
+enum class EMoverMode: uint8
+{
+	Lerp UMETA(DisplayName = "Lerp"),
+	VInterpTo UMETA(DisplayName = "VInterpTo"),
+	VInterpConstantTo UMETA(DisplayName = "VInterpConstantTo")
+};
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class CRYPTRAIDER_API UMover : public UActorComponent
@@ -24,17 +32,28 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	void TriggerMovement();
-
+	
 private:
 	UPROPERTY(EditAnywhere, Category="MovingSettings")
 	FVector MoveOffset;
 
-	UPROPERTY(EditAnywhere, Category="MovingSettings")
+	UPROPERTY(EditAnywhere, Category="MovingSettings", meta=(Tooltip="The time it takes to move for constant speed."))
 	float MoveTime = 2.0f;
 
+	UPROPERTY(EditAnywhere, Category="MovingSettings", meta=(Tooltip="Choose the movement type."))
+	EMoverMode MovementType = EMoverMode::VInterpConstantTo;
+	
+	UPROPERTY(EditAnywhere, Category="MovingSettings", meta=(Tooltip="The speed when using VInterpTo."))
+	float MoveSpeed = 5.0f;
+	
 	bool bIsMoving = false;
-	float MoveSpeed;
+	float ConstantMoveSpeed;
 	FTimerHandle MoveTimerHandle;
+	FVector StartLocation;
+
+	// Temp, for timer
+	double StartTime;
 
 	void MoveStep(const FVector& TargetLocation);
+	void MoveStep(const FVector& TargetLocation, TSharedPtr<float> DeltaTime);
 };
