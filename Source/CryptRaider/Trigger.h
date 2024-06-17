eed4e120fa3ff24a8cde688b9e5fe59cd84289db
Zihan Yang <zihan_yang_1996@outlook.com>
2024-06-17 19:07:40 -0400

    Make the stand a blueprint class, add a C++ boxcomponent as the trigger that will be used to trigger the movement of a hidden wall.

---
+++ Source/CryptRaider/Trigger.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "Trigger.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRYPTRAIDER_API UTrigger : public UBoxComponent
+{
+	GENERATED_BODY()
+	
+};
